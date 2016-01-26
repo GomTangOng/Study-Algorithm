93993b0ea6f1e401be307e9cc7a05f9719871605
kk <smrl7460@naver.com>
2016-01-26 09:44:31 +0900

    main test

--- Algorithm/main.cpp
+++ Algorithm/main.cpp
@@ -3,23 +3,7 @@
 
 int main()
 {
-	CRingBuffer *buf = new CRingBuffer();
-	char temp[100];
-	while (1)
-	{
-		scanf("%s", temp);
-		int num = buf->Enqueue(temp, strlen(temp));
-		if (num == -1)
-		{
-			buf->Dequeue(temp, 20);
-			ZeroMemory(temp, sizeof(temp));
-		}
-		else
-		{
-			ZeroMemory(temp, sizeof(temp));
-		}		
-		printf("Enequeue(CurrentSize : %d) : %s\n", buf->GetCurrentSize(), buf->GetQueueBuffer());
-	}
+
 
 	return 0;
 }
